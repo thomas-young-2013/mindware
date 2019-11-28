a038c0bcd26c0258ecaec6cff808fe13cbd9beb6
thomas-young-2013 <1225646303@qq.com>
2019-11-28 14:13:13 +0100

    update

--- evaluate_feature_engineering.py
+++ evaluate_feature_engineering.py
@@ -25,7 +25,7 @@ dataset_list = 'credit,diabetes,pc4,sick,spectf,splice,waveform,' \
 dataset_list2 = 'eeg,higgs,kropt,madelon,mushroom,quake,satimage,semeion'
 dataset_list3 = 'messidor_features,lymphography,winequality_red,winequality_white,credit,' \
                 'ionosphere,splice,diabetes,pc4,spectf,spambase,amazon_employee'
-parser.add_argument('--datasets', type=str, default='ionosphere')
+parser.add_argument('--datasets', type=str, default=dataset_list3)
 args = parser.parse_args()
 
 
