0797d453bb4f3e0ef6dbb31481ee5cd2669686d9
shenyu <459240868@qq.com>
2020-03-01 15:43:57 +0800

    update overfit

--- test/exps/evaluate_fe_hpo_overfitting.py
+++ test/exps/evaluate_fe_hpo_overfitting.py
@@ -3,23 +3,26 @@ import pickle
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--dataset', type=str, default='cmc')
-parser.add_argument('--algo', type=str, default='adaboost,libsvm_svc,random_forest,xgradient_boosting')
+parser.add_argument('--algo', type=str, default='k_nearest_neighbors,libsvm_svc,random_forest,xgradient_boosting')
 data_dir = '/Users/thomasyoung/Desktop/overfitting/overfit/'
 
 
 def get_avg(dataset, algo, arm_type):
-    val_result, test_result = list(), list()
-    for _id in range(5):
+    train_result, val_result, test_result = [], [], []
+    for _id in range(10):
         data_path = data_dir + '%s-%s-%s-100-%d.pkl' % (arm_type, dataset, algo, _id)
         if os.path.exists(data_path):
             with open(data_path, 'rb') as f:
                 data = pickle.load(f)
-                val_result.append(data[0])
-                test_result.append(data[1])
-    val_result, test_result = np.mean(val_result, axis=0), np.mean(test_result, axis=0)
-    return val_result, test_result
+                train_result.append(data[0])
+                val_result.append(data[1])
+                test_result.append(data[2])
+    train_result, val_result, test_result = np.mean(train_result, axis=0), np.mean(val_result, axis=0), np.mean(
+        test_result, axis=0)
+    return train_result, val_result, test_result
 
 
 if __name__ == "__main__":
@@ -30,20 +33,24 @@ if __name__ == "__main__":
 
     for i in range(4):
         algo = algos[i]
-        plt.subplot(2, 2, i+1)
-        val_fe, test_fe = get_avg(dataset, algo, 'fe')
-        val_hpo, test_hpo = get_avg(dataset, algo, 'hpo')
+        plt.subplot(2, 2, i + 1)
+        train_fe, val_fe, test_fe = get_avg(dataset, algo, 'fe')
+        train_hpo, val_hpo, test_hpo = get_avg(dataset, algo, 'hpo')
+        print(train_fe)
         print(val_fe)
         print(test_fe)
+        print(train_hpo)
         print(val_hpo)
         print(test_hpo)
         x = np.linspace(1, 100, 100, endpoint=True)
+        plt.plot(x, train_fe, color="red", label='fe_train', linestyle=':')
         plt.plot(x, val_fe, color="red", label='fe_val', linestyle='--')
         plt.plot(x, test_fe, color="red", label='fe_test')
 
+        plt.plot(x, train_hpo, color="blue", label='hpo_train', linestyle=':')
         plt.plot(x, val_hpo, color="blue", label='hpo_val', linestyle='--')
         plt.plot(x, test_hpo, color="blue", label='hpo_test')
         plt.title('%s - %s' % (dataset, algo))
         plt.legend(loc='upper left', frameon=False)
 
-    plt.show()
+        plt.show()
--- test/exps/evaluate_overfitting.py
+++ test/exps/evaluate_overfitting.py
@@ -39,20 +39,38 @@ def conduct_hpo(dataset='pc4', classifier_id='random_forest', iter_num=100, run_
         output_dir='logs', per_run_time_limit=180
     )
     task_id = 'hpo-%s-%s-%d' % (dataset, classifier_id, iter_num)
-    val_acc_list, test_acc_list = [], []
+    train_acc_list, val_acc_list, test_acc_list = [], [], []
     for _iter in range(iter_num):
         perf, _, config = optimizer.iterate()
         val_acc_list.append(perf)
         estimator = fetch_predict_estimator(config, raw_data.data[0], raw_data.data[1])
+        pred = estimator.predict(raw_data.data[0])
+        train_perf = accuracy_score(raw_data.data[1], pred)
+        train_acc_list.append(train_perf)
         pred = estimator.predict(test_raw_data.data[0])
         test_perf = accuracy_score(test_raw_data.data[1], pred)
         test_acc_list.append(test_perf)
+        print(train_acc_list)
         print(val_acc_list)
         print(test_acc_list)
 
     save_path = save_dir + '%s-%d.pkl' % (task_id, run_id)
     with open(save_path, 'wb') as f:
-        pickle.dump([val_acc_list, test_acc_list], f)
+        pickle.dump([train_acc_list, val_acc_list, test_acc_list], f)
+
+    train_iter_list, val_acc_list = [], []
+    configs = optimizer.configs
+    perfs = optimizer.perfs
+    for i, config in enumerate(configs):
+        val_acc_list.append(perfs[i])
+        estimator = fetch_predict_estimator(config, raw_data.data[0], raw_data.data[1])
+        pred = estimator.predict(raw_data.data[0])
+        train_perf = accuracy_score(raw_data.data[1], pred)
+        train_iter_list.append(train_perf)
+
+    save_path = save_dir + 'iter-%s-%d.pkl' % (task_id, run_id)
+    with open(save_path, 'wb') as f:
+        pickle.dump([train_acc_list, val_acc_list, test_acc_list], f)
 
 
 def conduct_fe(dataset='pc4', classifier_id='random_forest', iter_num=100, run_id=0, seed=1):
@@ -68,26 +86,31 @@ def conduct_fe(dataset='pc4', classifier_id='random_forest', iter_num=100, run_i
     evaluator = Evaluator(default_config, name='fe', data_node=raw_data,
                           resampling_strategy='cv', seed=seed)
 
-    optimizer = EvaluationBasedOptimizer(task_type='classification', input_data=raw_data, evaluator=evaluator, model_id=classifier_id,
+    optimizer = EvaluationBasedOptimizer(task_type='classification', input_data=raw_data, evaluator=evaluator,
+                                         model_id=classifier_id,
                                          time_limit_per_trans=240, mem_limit_per_trans=10000, seed=seed)
 
     task_id = 'fe-%s-%s-%d' % (dataset, classifier_id, iter_num)
-    val_acc_list, test_acc_list = [], []
+    train_acc_list, val_acc_list, test_acc_list = [], [], []
     for _iter in range(iter_num):
         perf, _, incubent = optimizer.iterate()
         val_acc_list.append(perf)
         train_node = optimizer.apply(raw_data, incubent)
         test_node = optimizer.apply(test_raw_data, incubent)
         estimator = fetch_predict_estimator(default_config, train_node.data[0], train_node.data[1])
+        pred = estimator.predict(train_node.data[0])
+        train_perf = accuracy_score(train_node.data[1], pred)
+        train_acc_list.append(train_perf)
         pred = estimator.predict(test_node.data[0])
         test_perf = accuracy_score(test_node.data[1], pred)
         test_acc_list.append(test_perf)
+        print(train_acc_list)
         print(val_acc_list)
         print(test_acc_list)
 
     save_path = save_dir + '%s-%d.pkl' % (task_id, run_id)
     with open(save_path, 'wb') as f:
-        pickle.dump([val_acc_list, test_acc_list], f)
+        pickle.dump([train_acc_list, val_acc_list, test_acc_list], f)
 
 
 def check_datasets(datasets):
