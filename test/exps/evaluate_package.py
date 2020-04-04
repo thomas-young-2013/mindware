ba578a78d5fa4a76ae59333ff662b6180a5fda07
thomas-young-2013 <1225646303@qq.com>
2020-04-04 18:06:34 +0800

    add package info

--- test/exps/evaluate_package.py
+++ test/exps/evaluate_package.py
@@ -3,7 +3,7 @@ from automlToolkit.estimators import Classifier
 
 
 def evaluate_package():
-    train_data, test_data = load_train_test_data('pc4', proj_dir='./')
+    train_data, test_data = load_train_test_data('pc4', data_dir='./')
     Classifier().fit(train_data)
 
 
