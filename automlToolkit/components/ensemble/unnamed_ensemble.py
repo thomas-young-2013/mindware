31dbbd0f3163185c336a8ca2c9988831e8e0eb61
12997 <1299799152@qq.com>
2020-04-08 19:45:02 +0800

    Add the function of how to choose base models and how to calculate the corresponding weights.

--- automlToolkit/components/ensemble/unnamed_ensemble.py
+++ automlToolkit/components/ensemble/unnamed_ensemble.py
@@ -1,15 +1,83 @@
 import numpy as np
+import pandas as pd
 from sklearn.metrics.scorer import _BaseScorer
 from automlToolkit.components.utils.constants import CLS_TASKS
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.metrics import accuracy_score
 
 
-def choose_base_models(predictions, labels, num_model):
+# def choose_base_models(predictions, labels, num_model):
+#     base_mask = [0] * len(predictions)
+#     return base_mask
+
+def choose_base_models(predictions, num_model, interval=20):
+    num_class = predictions.shape[2]
+    num_total_models = predictions.shape[0]
     base_mask = [0] * len(predictions)
+    bucket = np.arange(interval+1) / interval
+    distribution = []
+    for prediction in predictions:
+        freq_array = []
+        for i in range(num_class):
+            class_i = prediction[:, i]
+            group = pd.cut(class_i, bucket, right=False)
+            counts = group.value_counts()
+            freq = list(counts / counts.sum())
+            freq_array += freq
+        distribution.append(freq_array)  # Shape: (num_total_models,20*num_class)
+    distribution = np.array(distribution)
+
+    # Apply the clustering algorithm
+    Model = AgglomerativeClustering(n_clusters=num_model, linkage="complete")
+    cluster = Model.fit(distribution)
+    """
+    Select models which are the most nearest to the clustering center
+    selected_models = []
+    """
+    for cluster_label in range(num_model):
+        cluster_center = np.zeros(distribution.shape[1])
+        count = 0
+        """
+         Averaging the distribution which belong the same clustering class
+          and then get the corresponding distribution center
+        """
+        for i in range(num_total_models):
+            if cluster.labels_[i] == cluster_label:
+                count += 1
+                cluster_center += distribution[i]
+        cluster_center = cluster_center / count
+        distances = np.sqrt(np.sum(np.asarray(cluster_center - distribution) ** 2, axis=1))
+        selected_model = distances.argmin()
+        base_mask[selected_model] = 1
+
     return base_mask
 
 
 def calculate_weights(predictions, labels, base_mask):
-    weights = [0] * len(predictions)
+    num_total_models = predictions.shape[0]
+    num_samples = predictions.shape[1]
+    weights = np.zeros((num_samples, num_total_models))
+    for i in range(num_total_models):
+        if base_mask[i] != 0:
+            predicted_labels = np.argmax(predictions[i], 1)
+            acc = accuracy_score(predicted_labels, labels)
+            model_weight = 0.5 * np.log(acc / (1 - acc))  # a concrete value
+            shannon_ent = -1.0 * np.sum(predictions[i] * np.log2(predictions[i]), 1)  # shape: (1, num_samples)
+            confidence = 1 / np.exp(shannon_ent)
+            model_weight = model_weight * confidence  # The weight of current model to all samples
+            model_weight = model_weight.reshape(num_samples, 1)
+            weights[:, i] = model_weight
+    return weights
+
+def calculate_weights_simple(predictions, labels, base_mask):
+    num_total_models = predictions.shape[0]
+    weights = [0]*num_total_models
+    for i in range(num_total_models):
+        if base_mask[i] != 0:
+            predicted_labels = np.argmax(predictions[i], 1)
+            acc = accuracy_score(predicted_labels, labels)
+            model_weight = 0.5 * np.log(acc / (1 - acc))  # a concrete value
+            weights[i] = model_weight
     return weights
 
 
