788f9cc37dde097099390e438e2d97b842343d9e
thomas-young-2013 <1225646303@qq.com>
2020-04-09 15:38:05 +0800

    update

--- test/exps/evaluate_1stlayer_bandit.py
+++ test/exps/evaluate_1stlayer_bandit.py
@@ -130,17 +130,19 @@ if __name__ == "__main__":
     np.random.seed(args.seed)
     seeds = np.random.randint(low=1, high=10000, size=start_id + args.rep_num)
 
-    algorithms = ['adaboost', 'random_forest',
-                  'libsvm_svc', 'sgd',
-                  'extra_trees', 'decision_tree',
-                  'liblinear_svc', 'k_nearest_neighbors',
-                  'passive_aggressive', 'xgradient_boosting',
-                  'lda', 'qda',
-                  'multinomial_nb', 'gaussian_nb', 'bernoulli_nb'
-                  ]
-    # algorithms = ['adaboost', 'random_forest',
-    #               'extra_trees', 'liblinear_svc', 'k_nearest_neighbors'
-    #               ]
+    if algo_num == 15:
+        algorithms = ['adaboost', 'random_forest',
+                      'libsvm_svc', 'sgd',
+                      'extra_trees', 'decision_tree',
+                      'liblinear_svc', 'k_nearest_neighbors',
+                      'passive_aggressive', 'xgradient_boosting',
+                      'lda', 'qda',
+                      'multinomial_nb', 'gaussian_nb', 'bernoulli_nb'
+                      ]
+    else:
+        algorithms = ['adaboost', 'random_forest',
+                      'extra_trees', 'liblinear_svc', 'k_nearest_neighbors'
+                      ]
 
     dataset_list = dataset_str.split(',')
 
