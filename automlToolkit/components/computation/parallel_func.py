09ffda6d27e75b32fca347967ecbd4ac59baac68
thomas-young-2013 <1225646303@qq.com>
2020-04-10 23:18:21 +0800

    add parallel_func

--- automlToolkit/components/computation/parallel_func.py
+++ automlToolkit/components/computation/parallel_func.py
@@ -6,13 +6,12 @@ from ConfigSpace import Configuration
 
 def execute_func(params):
     start_time = time.time()
-    evaluator, config = params
+    evaluator, config, subsample_ratio = params
     try:
         if isinstance(config, Configuration):
-            evaluator(config)
-            score = evaluator(config, name='hpo')
+            score = evaluator(config, name='hpo', data_subsample_ratio=subsample_ratio)
         else:
-            score = evaluator(config, data_node=config, name='fe')
+            score = evaluator(None, data_node=config, name='fe', data_subsample_ratio=subsample_ratio)
     except Exception as e:
         score = -np.inf
 
@@ -20,36 +19,42 @@ def execute_func(params):
     return score, time_taken
 
 
-def wait_tasks_finish(trial_stats):
-    all_completed = False
-    while not all_completed:
-        all_completed = True
-        for trial in trial_stats:
-            if not trial.done():
-                all_completed = False
-                time.sleep(0.1)
-                break
+class ParallelExecutor(object):
+    def __init__(self, evaluator, n_worker=1):
+        self.evaluator = evaluator
+        self.n_worker = n_worker
+        self.thread_pool = ThreadPoolExecutor(max_workers=n_worker)
 
+    def update_evaluator(self, evaluator):
+        self.evaluator = evaluator
 
-def parallel_execute(evaluator, param_list, n_worker=1):
-    thread_pool = ThreadPoolExecutor(max_workers=n_worker)
-    n_configuration = len(param_list)
-    batch_size = n_worker
-    n_batch = n_configuration // batch_size + (1 if n_configuration % batch_size != 0 else 0)
-    evaluation_result = list()
+    def wait_tasks_finish(self, trial_stats):
+        all_completed = False
+        while not all_completed:
+            all_completed = True
+            for trial in trial_stats:
+                if not trial.done():
+                    all_completed = False
+                    time.sleep(0.1)
+                    break
 
-    for i in range(n_batch):
-        execution_stats = list()
-        for _param in param_list[i * batch_size: (i + 1) * batch_size]:
-            execution_stats.append(thread_pool.submit(execute_func,
-                                   (evaluator, _param)))
-        # wait a batch of trials finish
-        wait_tasks_finish(execution_stats)
+    def parallel_execute(self, param_list, subsample_ratio=1.):
+        n_configuration = len(param_list)
+        batch_size = self.n_worker
+        n_batch = n_configuration // batch_size + (1 if n_configuration % batch_size != 0 else 0)
+        evaluation_result = list()
 
-        # get the evaluation statistics
-        for trial in execution_stats:
-            assert (trial.done())
-            perf = trial.result()
-            evaluation_result.append(perf)
-    thread_pool.shutdown(wait=True)
-    return evaluation_result
+        for i in range(n_batch):
+            execution_stats = list()
+            for _param in param_list[i * batch_size: (i + 1) * batch_size]:
+                execution_stats.append(self.thread_pool.submit(execute_func,
+                                       (self.evaluator, _param, subsample_ratio)))
+            # wait a batch of trials finish
+            self.wait_tasks_finish(execution_stats)
+
+            # get the evaluation statistics
+            for trial in execution_stats:
+                assert (trial.done())
+                perf = trial.result()
+                evaluation_result.append(perf)
+        return evaluation_result
