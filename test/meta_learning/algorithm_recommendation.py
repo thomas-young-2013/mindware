5c5760943645e7a97660e0c193dbc295ca90c859
thomas-young-2013 <1225646303@qq.com>
2020-05-02 04:43:05 +0800

    update meta-learner

--- test/meta_learning/algorithm_recommendation.py
+++ test/meta_learning/algorithm_recommendation.py
@@ -4,9 +4,17 @@ import sys
 sys.path.append(os.getcwd())
 from automlToolkit.components.meta_learning.algorithm_recomendation.algorithm_advisor import AlgorithmAdvisor
 from automlToolkit.components.meta_learning.algorithm_recomendation.meta_generator import get_feature_vector
+from automlToolkit.components.utils.constants import MULTICLASS_CLS
 
-alad = AlgorithmAdvisor().fit_meta_learner()
-meta_feature = get_feature_vector('pc4')
-algorithms, preds = alad.predict_meta_learner(meta_feature)
-print(dict(zip(algorithms, preds)))
-print(alad.fetch_algorithm_set('pc4'))
+alad = AlgorithmAdvisor(task_type=MULTICLASS_CLS)
+meta_infos = alad.fit_meta_learner()
+datasets = [item[0] for item in meta_infos]
+print(datasets)
+test_datasets = ['page-blocks(2)', 'page-blocks(1)']
+for test_dataset in test_datasets:
+    print(test_dataset in datasets)
+
+    meta_feature = get_feature_vector(test_dataset, task_type=MULTICLASS_CLS)
+    algorithms, preds = alad.predict_meta_learner(meta_feature)
+    print(dict(zip(algorithms, preds)))
+    print(alad.fetch_algorithm_set(test_dataset))
