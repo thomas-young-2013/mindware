135af490679ae25cdc425d1b709570ebd9548a0a
12997 <1299799152@qq.com>
2020-05-08 19:55:08 +0800

    Dealing the circumstance when the number of minority class is less than the value of k_neighbour in SMOTEENN

--- automlToolkit/components/feature_engineering/transformations/preprocessor/to_balanced.py
+++ automlToolkit/components/feature_engineering/transformations/preprocessor/to_balanced.py
@@ -1,7 +1,7 @@
 from automlToolkit.components.feature_engineering.transformations.base_transformer import *
 from automlToolkit.utils.functions import is_unbalanced_dataset
 from imblearn.combine import SMOTEENN
-
+from imblearn.over_sampling import SMOTE
 
 class DataBalancer(Transformer):
     def __init__(self):
@@ -11,7 +11,17 @@ class DataBalancer(Transformer):
         X, y = input_datanode.data
 
         if self.model is None:
-            self.model = SMOTEENN(random_state=self.random_state)
+            labels = list(y)
+            cnts = list()
+            for val in set(labels):
+                cnts.append(labels.count(val))
+            cnts = sorted(cnts)
+            if cnts[0] < 6:
+                sm = SMOTE(random_state=self.random_state, k_neighbors=cnts[0]-1)
+                self.model = SMOTEENN(random_state=self.random_state, smote=sm)
+            else:
+                # The default value of k_neighbors in SMOTEENN is 5
+                self.model = SMOTEENN(random_state=self.random_state)
 
         if is_unbalanced_dataset(input_datanode):
             X_res, y_res = self.model.fit_resample(X, y)
