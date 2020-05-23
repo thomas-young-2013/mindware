e2daf4be26460294b985b7f131843f8b0a662f71
thomas-young-2013 <1225646303@qq.com>
2020-05-23 17:35:31 +0800

    update travis

--- ci_scripts/unittests.sh
+++ ci_scripts/unittests.sh
@@ -1,3 +1,13 @@
-echo Testing revision $(git rev-parse HEAD) ...
-echo Testing from directory `pwd`
-conda list
+cd examples/ci_examples
+
+files=$(ls $folder)
+for file in $files
+do
+    echo "Begin to run $file"
+    python $file
+    rval=$?
+    if [ "$rval" != 0 ]; then
+        echo "Error running example $file"
+        exit $rval
+    fi
+done
