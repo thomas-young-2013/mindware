46294a310647bbc31f6e7762a7d7fb0b0ef40a8d
thomas-young-2013 <1225646303@qq.com>
2020-06-21 23:58:21 +0800

    update dl pipeline

--- solnml/autodl.py
+++ solnml/autodl.py
@@ -1,16 +1,16 @@
 import os
 import time
 import numpy as np
+from ConfigSpace.hyperparameters import UnParametrizedHyperparameter
 from solnml.components.utils.constants import IMG_CLS
+from solnml.datasets.image_dataset import BaseDataset
 from solnml.components.metrics.metric import get_metric
 from solnml.utils.logging_utils import setup_logger, get_logger
 from solnml.components.ensemble import ensemble_list
 from solnml.components.hpo_optimizer import build_hpo_optimizer
 from solnml.components.models.img_classification import _classifiers as _img_classifiers
-
+from solnml.components.evaluators.img_cls_evaluator import get_estimator_with_parameters
 from solnml.components.evaluators.img_cls_evaluator import ImgClassificationEvaluator
-from ConfigSpace.hyperparameters import UnParametrizedHyperparameter
-from solnml.datasets.image_dataset import BaseDataset
 
 img_classification_algorithms = _img_classifiers.keys()
 
@@ -66,6 +66,12 @@ class AutoDL(object):
             raise ValueError("%s is not supported for ensemble!" % ensemble_method)
 
         self.solvers = dict()
+        self.evaluators = dict()
+        # Single model.
+        self.best_algo_id = None
+        self.best_algo_config = None
+        # Ensemble models.
+        self.candidate_algo_ids = None
 
     def _get_logger(self, name):
         logger_name = 'SolnML-%s(%d)' % (name, self.seed)
@@ -76,39 +82,54 @@ class AutoDL(object):
 
     def fit(self, train_data: BaseDataset):
         _start_time = time.time()
-        # Fetch hyperparameter space.
         from solnml.components.models.img_classification import _classifiers, _addons
-        estimator_id = 'resnext'
-        if estimator_id in _classifiers:
-            clf_class = _classifiers[estimator_id]
-        elif estimator_id in _addons.components:
-            clf_class = _addons.components[estimator_id]
-        else:
-            raise ValueError("Algorithm %s not supported!" % estimator_id)
+        for estimator_id in self.include_algorithms:
+            if estimator_id in _classifiers:
+                clf_class = _classifiers[estimator_id]
+            elif estimator_id in _addons.components:
+                clf_class = _addons.components[estimator_id]
+            else:
+                raise ValueError("Algorithm %s not supported!" % estimator_id)
 
-        cs = clf_class.get_hyperparameter_search_space()
-        model = UnParametrizedHyperparameter("estimator", estimator_id)
-        cs.add_hyperparameter(model)
-        config_space = cs
-        default_config = cs.get_default_configuration()
-        config_space.seed(self.seed)
+            cs = clf_class.get_hyperparameter_search_space()
+            model = UnParametrizedHyperparameter("estimator", estimator_id)
+            cs.add_hyperparameter(model)
+            config_space = cs
+            default_config = cs.get_default_configuration()
+            config_space.seed(self.seed)
 
-        hpo_evaluator = ImgClassificationEvaluator(default_config, scorer=self.metric,
-                                                   dataset=train_data,
-                                                   seed=self.seed)
-        optimizer = build_hpo_optimizer(self.evaluation_type, hpo_evaluator, cs,
-                                        output_dir=self.output_dir,
-                                        per_run_time_limit=100000,
-                                        trials_per_iter=1,
-                                        seed=self.seed, n_jobs=self.n_jobs)
+            hpo_evaluator = ImgClassificationEvaluator(default_config, scorer=self.metric,
+                                                       dataset=train_data,
+                                                       seed=self.seed)
+            optimizer = build_hpo_optimizer(self.evaluation_type, hpo_evaluator, cs,
+                                            output_dir=self.output_dir,
+                                            per_run_time_limit=100000,
+                                            trials_per_iter=1,
+                                            seed=self.seed, n_jobs=self.n_jobs)
 
-        # Control flow.
-        if self.trial_num is None:
-            while time.time() <= _start_time + self.time_limit:
-                optimizer.iterate()
-        else:
-            for _ in self.trial_num:
-                optimizer.iterate()
+            # Control flow.
+            if self.trial_num is None:
+                while time.time() <= _start_time + self.time_limit:
+                    optimizer.iterate()
+            else:
+                for _ in self.trial_num:
+                    optimizer.iterate()
+            self.solvers[estimator_id] = optimizer
+            self.evaluators[estimator_id] = hpo_evaluator
+
+        # Best model id.
+        best_scores_ = list()
+        for estimator_id in self.include_algorithms:
+            if estimator_id in self.solvers:
+                solver_ = self.solvers[estimator_id]
+                best_scores_.append(np.max(solver_.perfs))
+            else:
+                best_scores_.append(-np.inf)
+        self.best_algo_id = self.include_algorithms[np.argmax(best_scores_)]
+        # Best model configuration.
+        solver_ = self.solvers[self.best_algo_id]
+        inc_idx = np.argmax(solver_.perfs)
+        self.best_algo_config = solver_.configs[inc_idx]
 
     def fetch_ensemble_members(self):
         stats = dict()
@@ -129,34 +150,18 @@ class AutoDL(object):
 
             hpo_eval_list = sorted(hpo_eval_dict.items(), key=lambda item: item[1], reverse=True)
             model_items = list()
-            combined_list = list()
 
             if len(hpo_eval_list) > 20:
                 idxs = np.arange(min_model_num) * leap
                 for idx in idxs:
                     model_items.append(hpo_eval_list[idx])
-                combined_list.extend(hpo_eval_list[min_model_num * leap:])
             else:
                 model_items.extend(hpo_eval_list[:min_model_num])
-                combined_list.extend(hpo_eval_list[min_model_num:])
-            # Sort the left configs.
-            combined_list = sorted(combined_list, key=lambda item: item[1], reverse=True)
-
-            left_model_num = model_num - 2 * min_model_num
-            if left_model_num > 0:
-                if len(combined_list) > 20:
-                    idxs = np.arange(left_model_num) * leap
-                    for idx in idxs:
-                        model_items.append(combined_list[idx])
-                else:
-                    model_items.extend(combined_list[:left_model_num])
 
             hpo_configs = [item[0][1] for item in model_items]
 
-            model_to_eval = []
-            # todo: what to store?
-            data['model_to_eval'] = model_to_eval
-            self.logger.info('%s, %d' % (algo_id, len(model_to_eval)))
+            data['model_configs'] = hpo_configs
+            self.logger.info('%s, %d' % (algo_id, len(hpo_configs)))
             stats[algo_id] = data
         self.logger.info('Preparing basic models finished.')
         return stats
@@ -168,7 +173,8 @@ class AutoDL(object):
         pass
 
     def predict(self, test_data: BaseDataset):
-        pass
+        model_ = get_estimator_with_parameters(self.best_algo_config)
+        return model_.predict(test_data)
 
     def score(self, test_data: BaseDataset, metric_func=None):
         if metric_func is None:
