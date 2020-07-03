1580c8acdbb91b6e8481338c312f5bed2585d3ac
thomas-young-2013 <1225646303@qq.com>
2020-07-03 11:14:22 +0800

    update dataset api

--- solnml/autodl.py
+++ solnml/autodl.py
@@ -3,6 +3,7 @@ import time
 import torch
 import numpy as np
 from ConfigSpace.hyperparameters import UnParametrizedHyperparameter
+from solnml.utils.constant import MAX_INT
 from solnml.components.utils.constants import IMG_CLS, TEXT_CLS, OBJECT_DET
 from solnml.datasets.base_dataset import BaseDataset
 from solnml.components.metrics.metric import get_metric
@@ -112,28 +113,57 @@ class AutoDL(object):
                      )
         return get_logger(logger_name)
 
+    def get_model_config_space(self, estimator_id):
+        if estimator_id in self._estimators:
+            clf_class = self._estimators[estimator_id]
+        elif estimator_id in self._addons.components:
+            clf_class = self._addons.components[estimator_id]
+        else:
+            raise ValueError("Algorithm %s not supported!" % estimator_id)
+
+        cs = clf_class.get_hyperparameter_search_space()
+        model = UnParametrizedHyperparameter("estimator", estimator_id)
+        cs.add_hyperparameter(model)
+        if self.task_type == IMG_CLS:
+            aug_space = get_aug_hyperparameter_space()
+            cs.add_hyperparameters(aug_space.get_hyperparameters())
+            cs.add_conditions(aug_space.get_conditions())
+        return cs
+
+    def profile_models(self, profile_epoch_n=1):
+        training_cost_per_unit = list()
+        default_training_epoch = list()
+        for estimator_id in self.include_algorithms:
+            cs = self.get_model_config_space(estimator_id)
+            default_config = cs.get_default_configuration()
+            assert 'epoch_num' in default_config
+            default_training_epoch.append(default_config['epoch_num'])
+            default_config['epoch_num'] = profile_epoch_n
+            cs.seed(self.seed)
+
+            hpo_evaluator = self.evaluators[estimator_id]
+            _start_time = time.time()
+            try:
+                hpo_evaluator(default_config)
+                training_cost_per_unit.append(time.time() - _start_time)
+            except Exception:
+                training_cost_per_unit.append(MAX_INT)
+
+        K = 5
+        estimator_list = list()
+        for id, estimator_id in enumerate(self.include_algorithms):
+            num_of_units = default_training_epoch[id] / profile_epoch_n
+            if training_cost_per_unit[id] * num_of_units * K < self.time_limit:
+                estimator_list.append(estimator_id)
+        return estimator_list
+
     def fit(self, train_data: BaseDataset):
         _start_time = time.time()
 
         for estimator_id in self.include_algorithms:
-            if estimator_id in self._estimators:
-                clf_class = self._estimators[estimator_id]
-            elif estimator_id in self._addons.components:
-                clf_class = self._addons.components[estimator_id]
-            else:
-                raise ValueError("Algorithm %s not supported!" % estimator_id)
-
-            cs = clf_class.get_hyperparameter_search_space()
-            model = UnParametrizedHyperparameter("estimator", estimator_id)
-            cs.add_hyperparameter(model)
-            config_space = cs
+            cs = self.get_model_config_space(estimator_id)
             default_config = cs.get_default_configuration()
-            config_space.seed(self.seed)
-
-            if self.task_type == IMG_CLS:
-                aug_space = get_aug_hyperparameter_space()
-                cs.add_hyperparameters(aug_space.get_hyperparameters())
-                cs.add_conditions(aug_space.get_conditions())
+            cs.seed(self.seed)
 
             hpo_evaluator = DLEvaluator(default_config,
                                         self.task_type,
@@ -146,16 +176,21 @@ class AutoDL(object):
                                             per_run_time_limit=100000,
                                             trials_per_iter=1,
                                             seed=self.seed, n_jobs=self.n_jobs)
-
-            # Control flow.
-            if self.trial_num is None:
-                while time.time() <= _start_time + self.time_limit:
-                    optimizer.iterate()
-            else:
-                for _ in self.trial_num:
-                    optimizer.iterate()
             self.solvers[estimator_id] = optimizer
             self.evaluators[estimator_id] = hpo_evaluator
+
+        algorithm_candidates = self.profile_models()
+
+        # Control flow via round robin.
+        n_algorithm = len(algorithm_candidates)
+        if self.trial_num is None:
+            algo_id = 0
+            while time.time() <= _start_time + self.time_limit:
+                self.solvers[algorithm_candidates[algo_id]].iterate()
+                algo_id = (algo_id + 1) % n_algorithm
+        else:
+            for id in self.trial_num:
+                self.solvers[algorithm_candidates[id % n_algorithm]].iterate()
 
         # Best model id.
         best_scores_ = list()
@@ -176,7 +211,7 @@ class AutoDL(object):
             return
 
         if self.ensemble_method is not None:
-            stats = self.fetch_ensemble_members()
+            stats = self.fetch_ensemble_members(algorithm_candidates)
 
             # Ensembling all intermediate/ultimate models found in above optimization process.
             self.es = EnsembleBuilder(stats=stats,
@@ -188,12 +223,10 @@ class AutoDL(object):
                                       output_dir=self.output_dir)
             self.es.fit(data=train_data)
 
-    def fetch_ensemble_members(self):
+    def fetch_ensemble_members(self, candidate_algorithms):
         stats = dict()
-        stats['candidate_algorithms'] = self.include_algorithms
-
         # a subset of included algorithms.
-        stats['include_algorithms'] = self.include_algorithms
+        stats['include_algorithms'] = candidate_algorithms
         stats['split_seed'] = self.seed
 
         self.logger.info('Choose basic models for ensemble stage.')
