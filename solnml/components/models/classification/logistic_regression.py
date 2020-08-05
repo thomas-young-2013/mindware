870bc17c900d6e6c2022df386cfa7ec3efa3909d
thomas-young-2013 <1225646303@qq.com>
2020-08-05 23:29:02 +0800

    update gb classifier and lr.

--- solnml/components/models/classification/logistic_regression.py
+++ solnml/components/models/classification/logistic_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ConfigSpace.configuration_space import ConfigurationSpace
 from ConfigSpace.hyperparameters import UniformFloatHyperparameter, \
-    CategoricalHyperparameter
+    CategoricalHyperparameter, UnParametrizedHyperparameter
 
 from solnml.components.models.base_model import BaseClassificationModel
 from solnml.components.utils.constants import DENSE, SPARSE, UNSIGNED_DATA, PREDICTIONS
@@ -57,31 +57,19 @@ class Logistic_Regression(BaseClassificationModel):
                 'output': (PREDICTIONS,)}
 
     @staticmethod
-    def get_hyperparameter_search_space(dataset_properties=None, optimizer='smac'):
-        if optimizer == 'smac':
-            C = UniformFloatHyperparameter("C", 0.03125, 10, log=True,
-                                           default_value=1.0)
-            tol = UniformFloatHyperparameter("tol", 1e-6, 1e-2, default_value=1e-4,
-                                             log=True)
+    def get_hyperparameter_search_space(dataset_properties=None):
+        C = UniformFloatHyperparameter("C", 0.03125, 10, log=True,
+                                       default_value=1.0)
+        tol = UniformFloatHyperparameter("tol", 1e-6, 1e-2, default_value=1e-4,
+                                         log=True)
 
-            max_iter = UniformFloatHyperparameter("max_iter", 100, 1000, q=100, default_value=100)
+        max_iter = UnParametrizedHyperparameter("max_iter", 10000)
 
-            penalty = CategoricalHyperparameter(name="penalty",
-                                                choices=["l1", "l2"],
-                                                default_value="l2")
-            solver = CategoricalHyperparameter(name="solver", choices=["liblinear", "saga"], default_value="liblinear")
+        penalty = CategoricalHyperparameter(name="penalty",
+                                            choices=["l1", "l2"],
+                                            default_value="l2")
+        solver = CategoricalHyperparameter(name="solver", choices=["liblinear", "saga"], default_value="liblinear")
 
-            cs = ConfigurationSpace()
-            cs.add_hyperparameters([C, penalty, solver, tol, max_iter])
-            return cs
-        elif optimizer == 'tpe':
-            from hyperopt import hp
-            space = {'C': hp.loguniform('lr_C', np.log(0.03125), np.log(10)),
-                     'tol': hp.loguniform('lr_tol', np.log(1e-6), np.log(1e-2)),
-                     'max_iter': hp.uniform('lr_max_iter', 100, 1000),
-                     'penalty': hp.choice('lr_penalty', ["l1", "l2"]),
-                     'solver': hp.choice('lr_solver', ["liblinear", "saga"])}
-
-            init_trial = {'C': 1, 'tol': 1e-4, 'max_iter': 100, 'penalty': "l2", 'solver': "liblinear"}
-
-            return space
+        cs = ConfigurationSpace()
+        cs.add_hyperparameters([C, penalty, solver, tol, max_iter])
+        return cs
