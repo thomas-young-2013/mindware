a732b360548b6c3af36a97887c519126a3b9f0d1
shenyu <459240868@qq.com>
2020-09-28 19:11:13 +0800

    update robust rescaler

--- solnml/components/feature_engineering/transformations/rescaler/robust.py
+++ solnml/components/feature_engineering/transformations/rescaler/robust.py
@@ -1,13 +1,17 @@
+from ConfigSpace.configuration_space import ConfigurationSpace
+from ConfigSpace.hyperparameters import UniformFloatHyperparameter
 from solnml.components.feature_engineering.transformations.base_transformer import *
 
 
 class RobustScaler(Transformer):
-    def __init__(self, **kwargs):
+    def __init__(self, q_min=0.25, q_max=0.75, **kwargs):
         super().__init__("robust_scaler", 44)
         self.input_type = [DISCRETE, NUMERICAL]
         self.compound_mode = 'in_place'
         self.output_type = NUMERICAL
-        self.ignore_flag = False
+
+        self.q_min = q_min
+        self.q_max = q_max
 
     @ease_trans
     def operate(self, input_data, target_fields):
@@ -16,20 +20,21 @@ class RobustScaler(Transformer):
         X_new = X[:, target_fields]
 
         if not self.model:
-            self.model = RobustScaler()
+            self.model = RobustScaler(quantile_range=(self.q_min, self.q_max))
             self.model.fit(X_new)
 
-        _flag = False
-        for _id in range(X_new.shape[1]):
-            _data = X_new[:, _id]
-            if (np.percentile(_data, 0.75) - np.percentile(_data, 0.25)) > 5.:
-                _flag = True
-        if not _flag:
-            self.ignore_flag = True
-
-        if not self.ignore_flag:
-            _X = self.model.transform(X_new)
-        else:
-            _X = X_new.copy()
+        _X = self.model.transform(X_new)
 
         return _X
+
+    @staticmethod
+    def get_hyperparameter_search_space(dataset_properties=None, optimizer='smac'):
+        cs = ConfigurationSpace()
+        q_min = UniformFloatHyperparameter(
+            'q_min', 0.001, 0.3, default_value=0.25
+        )
+        q_max = UniformFloatHyperparameter(
+            'q_max', 0.7, 0.999, default_value=0.75
+        )
+        cs.add_hyperparameters((q_min, q_max))
+        return cs
