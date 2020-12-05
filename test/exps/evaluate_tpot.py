5337a3039544a99a2664a56d559ef3aea769ecce
shenyu <459240868@qq.com>
2020-12-05 22:03:20 +0800

    update test script

--- test/exps/evaluate_tpot.py
+++ test/exps/evaluate_tpot.py
@@ -4,11 +4,12 @@ import pickle
 import argparse
 import numpy as np
 import time
-from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import balanced_accuracy_score, mean_squared_error
 
 sys.path.append(os.getcwd())
-from tpot import TPOTClassifier
+from tpot import TPOTClassifier, TPOTRegressor
 from solnml.datasets.utils import load_data, load_train_test_data
+from solnml.components.utils.constants import MULTICLASS_CLS, REGRESSION
 
 parser = argparse.ArgumentParser()
 dataset_set = 'diabetes,spectf,credit,ionosphere,lymphography,pc4,' \
@@ -19,14 +20,15 @@ parser.add_argument('--start_id', type=int, default=0)
 parser.add_argument('--time_cost', type=int, default=600)
 parser.add_argument('--n_job', type=int, default=1)
 parser.add_argument('--seed', type=int, default=1)
+parser.add_argument('--task_type', type=str, default='cls', choices=['cls', 'rgs'])
 
-max_eval_time = 10
+max_eval_time = 5
 save_dir = './data/exp_sys/'
 if not os.path.exists(save_dir):
     os.makedirs(save_dir)
 
 
-def evaluate_tpot(dataset, run_id, time_limit, seed=1, use_fe=True):
+def evaluate_tpot(dataset, task_type, run_id, time_limit, seed=1, use_fe=True):
     n_job = args.n_job
     # Construct the ML model.
     config = None
@@ -34,14 +36,24 @@ def evaluate_tpot(dataset, run_id, time_limit, seed=1, use_fe=True):
         from solnml.utils.tpot_config import classifier_config_dict
         config = classifier_config_dict
 
-    automl = TPOTClassifier(config_dict=config, generations=10000, population_size=20,
-                            verbosity=2, n_jobs=n_job, cv=0.2,
-                            scoring='balanced_accuracy',
-                            max_eval_time_mins=max_eval_time,
-                            max_time_mins=int(time_limit / 60),
-                            random_state=seed)
+    _task_type = MULTICLASS_CLS if task_type == 'cls' else REGRESSION
 
-    raw_data, test_raw_data = load_train_test_data(dataset, task_type=1)
+    if task_type == 'cls':
+        automl = TPOTClassifier(config_dict=config, generations=10000, population_size=20,
+                                verbosity=2, n_jobs=n_job, cv=0.2,
+                                scoring='balanced_accuracy',
+                                max_eval_time_mins=max_eval_time,
+                                max_time_mins=int(time_limit / 60),
+                                random_state=seed)
+    else:
+        automl = TPOTRegressor(config_dict=config, generations=10000, population_size=20,
+                               verbosity=2, n_jobs=n_job, cv=0.2,
+                               scoring='neg_mean_squared_error',
+                               max_eval_time_mins=max_eval_time,
+                               max_time_mins=int(time_limit / 60),
+                               random_state=seed)
+
+    raw_data, test_raw_data = load_train_test_data(dataset, task_type=_task_type)
     X_train, y_train = raw_data.data
     X_test, y_test = test_raw_data.data
     X_train, y_train = X_train.astype('float64'), y_train.astype('int')
@@ -50,15 +62,22 @@ def evaluate_tpot(dataset, run_id, time_limit, seed=1, use_fe=True):
     automl.fit(X_train, y_train)
     y_hat = automl.predict(X_test)
     pareto_front = automl._pareto_front
-    valid_accuracy = max([pareto_front.keys[x].wvalues[1] for x in range(len(pareto_front.keys))])
-    test_accuracy = balanced_accuracy_score(y_test, y_hat)
-    print("%d-th Evaluation: Valid accuracy score => %.4f" % (run_id, valid_accuracy))
-    print("%d-th Evaluation: Test accuracy score => %.4f" % (run_id, test_accuracy))
+
+    if task_type == 'cls':
+        score_func = balanced_accuracy_score
+    else:
+        score_func = mean_squared_error
+
+    valid_score = max([pareto_front.keys[x].wvalues[1] for x in range(len(pareto_front.keys))])
+    test_score = score_func(y_test, y_hat)
+    print('Run ID         : %d' % run_id)
+    print('Dataset        : %s' % dataset)
+    print('Val/Test score : %f - %f' % (valid_score, test_score))
     scores = automl.scores
     times = automl.times
-    save_path = save_dir + 'cls_tpot_%s_false_%d_1_%d.pkl' % (dataset, time_limit, run_id)
+    save_path = save_dir + '%s_tpot_%s_false_%d_1_%d.pkl' % (task_type, dataset, time_limit, run_id)
     with open(save_path, 'wb') as f:
-        pickle.dump([dataset, valid_accuracy, test_accuracy, times, scores, start_time], f)
+        pickle.dump([dataset, valid_score, test_score, times, scores, start_time], f)
 
 
 if __name__ == "__main__":
@@ -67,6 +86,7 @@ if __name__ == "__main__":
     time_limit = args.time_cost
     start_id = args.start_id
     rep = args.rep_num
+    task_type = args.task_type
     np.random.seed(args.seed)
     seeds = np.random.randint(low=1, high=10000, size=start_id + args.rep_num)
 
@@ -79,4 +99,4 @@ if __name__ == "__main__":
     for dataset in dataset_list:
         for run_id in range(start_id, start_id + rep):
             seed = int(seeds[run_id])
-            evaluate_tpot(dataset, run_id, time_limit, seed)
+            evaluate_tpot(dataset, task_type, run_id, time_limit, seed)
